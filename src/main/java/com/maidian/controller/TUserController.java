7a0bf1488beca880836d7e7ed22b4a18c4644066
linan.wang <linan.wang@ly.com>
2021-02-20 09:52:43 +0800

    add index

--- src/main/java/com/maidian/controller/TUserController.java
+++ src/main/java/com/maidian/controller/TUserController.java
@@ -6,11 +6,7 @@ import com.maidian.service.impl.TUserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,19 +19,24 @@ import java.util.List;
  * @since 2021-02-19
  */
 @RestController
-@RequestMapping("/t-user")
+//@RequestMapping("/t-user")
 @Slf4j
-
 public class TUserController {
-
 
     @Autowired
     @Qualifier("TUserServiceImpl")
     private TUserService tUserService;
 
-    @GetMapping("/findAllUser")
+    @GetMapping("findAllUser")
+    @ResponseBody
     public List<TUser> findAllUser(){
         return tUserService.findAllUser();
+    }
+
+    @GetMapping("index")
+    @ResponseBody
+    public String index(){
+        return "{'name':'xiaoming','age':'13'}";
     }
 
 }
